dbeccca211583cd78b3c8835c80e15ee006ac1b8
Shava71 <demdel2100@mail.ru>
2025-11-02 20:32:43 +0300

    fix roles data

--- Services/WorkerService/WorkerService.DAL/Migrations/20251102170031_fix1.Designer.cs
+++ Services/WorkerService/WorkerService.DAL/Migrations/20251102170031_fix1.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WorkerService.DAL.Data;
@@ -11,9 +12,11 @@ using WorkerService.DAL.Data;
 namespace WorkerService.DAL.Migrations
 {
     [DbContext(typeof(WorkerDbContext))]
-    partial class WorkerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251102170031_fix1")]
+    partial class fix1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Services/WorkerService/WorkerService.DAL/Migrations/20251102170031_fix1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkerService.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Worker",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Worker",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Worker");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Worker");
+        }
+    }
+}
